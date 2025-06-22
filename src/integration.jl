7b2d0a28760acb3fac31d326664da68aceac709b
Bumblebee00 <mikamatto03@gmail.com>
2025-06-22 15:10:13 +0200

    added tests in each_rule_tests file

--- src/integration.jl
+++ src/integration.jl
@@ -4,7 +4,8 @@ function apply_rule(integrand; verbose = false)
     for (i, rule) in enumerate(rules)
         result = rule(integrand)
         if result !== nothing
-            verbose && printstyled("┌---Applied rule $i:\n| ", join(split(string(rule), '\n'), "\n| "), "\n└---with result: ", result, "\n"; color = :light_blue)
+            verbose && printstyled("┌---Applied rule $i:\n| ", join(split(string(rule), '\n'), "\n| "), "\n└---with result: "; color = :light_blue)
+            verbose && printstyled( result, "\n"; color = :light_blue, reverse=true)
             return result
         end
     end
@@ -13,16 +14,34 @@ function apply_rule(integrand; verbose = false)
     return integrand
 end
 
+# TODO: there is a inefficiency here. when nodes like (..1.)*∫(..2..) are found, 
+# true is returned, all rules are checked again, without succes, only
+# after the prewalk checks the children of the expressions ..1.. and
+# ∫(..2..) Needs to be modified to check diectly the integrand
 function shouldtransform(node; verbose = false)
     verbose && print("Checking node ", node, "...")
     if !SymbolicUtils.iscall(node)
         verbose && println(" is not a tree, skipping branch.")
         return false
     end
-
-    cond = SymbolicUtils.operation(node) === ∫
-    verbose && println(" is a tree ", cond ? "and is a ∫" : "but not a ∫, skipping branch.")
-    return cond
+    
+    
+    node_op = SymbolicUtils.operation(node)
+    # search for nodes ∫(...)
+    if node_op === ∫
+        verbose && println("Is a tree with ∫ operation, applying rules")
+        return true
+        # search for nodes (...)*∫(...) it is intentionally not recursive, but checks only one level deep
+    elseif node_op === *
+        for a in arguments(node)
+            if SymbolicUtils.iscall(a) && operation(a)===∫
+                verbose && println("Is a tree like (...)*∫(...) applying rules (inefficiency)")
+                return true
+            end
+        end
+    end
+    verbose && println(" is a tree but not a ∫, skipping branch.")
+    return false
 end 
 
 # Prewalk from SymbolicUtils.Rewriters is used, to explore the tree of the 
@@ -32,10 +51,11 @@ end
 # previous rules, in case a rule transforms the integral in another integral
 # (for example linearity rules). 
 function integrate(integrand, int_var; verbose = false)
-    conditional = IfElse(x -> shouldtransform(x; verbose=verbose), 
-                        x -> apply_rule(x; verbose=verbose), 
-                        Empty())
-    return Prewalk(conditional)(∫(integrand,int_var))
+    rewriter = If(
+        x -> shouldtransform(x; verbose=verbose), 
+        x -> apply_rule(x; verbose=verbose)
+    )
+    return Prewalk(rewriter)(∫(integrand,int_var))
 end
 
 # If no integration variable provided
