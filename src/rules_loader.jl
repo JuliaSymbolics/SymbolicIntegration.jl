094c95e7a1fbf8cff8b9a2af3de1558d9e4bb159
Bumblebee00 <mikamatto03@gmail.com>
2025-07-03 00:54:00 +0200

    refactored rules names

--- src/rules_loader.jl
+++ src/rules_loader.jl
@@ -1,19 +1,19 @@
 # Load all rules from the IntegrationRules directory
 function load_all_rules()
     rules_paths = [
-    "IntegrationRules/9 Miscellaneous/9.1 Integrand simplification rules.jl"
-    "IntegrationRules/1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.1 (a+b x)^m.jl"
-    "IntegrationRules/1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.2 (a+b x)^m (c+d x)^n.jl"
-    "IntegrationRules/1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.3 (a+b x)^m (c+d x)^n (e+f x)^p.jl"
-    "IntegrationRules/1 Algebraic functions/1.1 Binomial products/1.1.3 General/1.1.3.1 (a+b x^n)^p.jl"
-    "IntegrationRules/2 Exponentials/2.1 (c+d x)^m (a+b (F^(g (e+f x)))^n)^p.jl"
-    "IntegrationRules/2 Exponentials/2.3 Miscellaneous exponentials.jl"
+    "9 Miscellaneous/9.1 Integrand simplification rules.jl"
+    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.1 (a+b x)^m.jl"
+    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.2 (a+b x)^m (c+d x)^n.jl"
+    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.3 (a+b x)^m (c+d x)^n (e+f x)^p.jl"
+    "1 Algebraic functions/1.1 Binomial products/1.1.3 General/1.1.3.1 (a+b x^n)^p.jl"
+    "2 Exponentials/2.1 (c+d x)^m (a+b (F^(g (e+f x)))^n)^p.jl"
+    "2 Exponentials/2.3 Miscellaneous exponentials.jl"
     ]
 
     all_rules = []
     identifiers = []
     for file in rules_paths
-        include(joinpath(@__DIR__, file))
+        include(joinpath(@__DIR__, "rules/" * file))
         file_identifiers = [x[1] for x in file_rules]
         rules = [x[2] for x in file_rules]
         append!(all_rules, rules)
