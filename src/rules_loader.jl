5574603c1cacc41f3cefc8158c67c4901c2de973
Bumblebee00 <mikamatto03@gmail.com>
2025-08-10 11:18:40 +0200

    moved all ruels array to SymbolicIntegration.jl for clarity

--- src/rules_loader.jl
+++ src/rules_loader.jl
@@ -24,75 +24,12 @@ function load_all_rules(rules_paths)
         append!(identifiers, [x[1] for x in file_rules])
     end
     print("\e[1A\e[2K\e[1A\e[2K")
+    println("Loaded $(length(rules)) rules from $(length(rules_paths)) files.")
 end
 
+# if called with no argument loads all rules from the default paths
 function load_all_rules()
-    rules_paths = [
-    "9 Miscellaneous/9.1 Integrand simplification rules.jl"
-    
-    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.1 (a+b x)^m.jl"
-    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.2 (a+b x)^m (c+d x)^n.jl"
-    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.3 (a+b x)^m (c+d x)^n (e+f x)^p.jl"
-    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.4 (a+b x)^m (c+d x)^n (e+f x)^p (g+h x)^q.jl"
-
-    "1 Algebraic functions/1.1 Binomial products/1.1.2 Quadratic/1.1.2.1 (a+b x^2)^p.jl"
-    "1 Algebraic functions/1.1 Binomial products/1.1.2 Quadratic/1.1.2.2 (c x)^m (a+b x^2)^p.jl"
-    "1 Algebraic functions/1.1 Binomial products/1.1.2 Quadratic/1.1.2.3 (a+b x^2)^p (c+d x^2)^q.jl"
-    "1 Algebraic functions/1.1 Binomial products/1.1.2 Quadratic/1.1.2.4 (e x)^m (a+b x^2)^p (c+d x^2)^q.jl"
-    # 5, 6, 7, 8, 9
-
-    "1 Algebraic functions/1.1 Binomial products/1.1.3 General/1.1.3.1 (a+b x^n)^p.jl"
-    "1 Algebraic functions/1.1 Binomial products/1.1.3 General/1.1.3.2 (c x)^m (a+b x^n)^p.jl"
-    # 3, 4, 5, 6
-
-    "1 Algebraic functions/1.2 Trinomial products/1.2.1 Quadratic/1.2.1.1 (a+b x+c x^2)^p.jl"
-    "1 Algebraic functions/1.2 Trinomial products/1.2.1 Quadratic/1.2.1.2 (d+e x)^m (a+b x+c x^2)^p.jl"
-    "1 Algebraic functions/1.2 Trinomial products/1.2.1 Quadratic/1.2.1.3 (d+e x)^m (f+g x) (a+b x+c x^2)^p.jl" # not most updated version?
-    "1 Algebraic functions/1.2 Trinomial products/1.2.1 Quadratic/1.2.1.4 (d+e x)^m (f+g x)^n (a+b x+c x^2)^p.jl" # not most updated version?
-    # 1.2.1.5
-
-    "1 Algebraic functions/1.2 Trinomial products/1.2.2 Quartic/1.2.2.1 (a+b x^2+c x^4)^p.jl"
-    "1 Algebraic functions/1.2 Trinomial products/1.2.2 Quartic/1.2.2.2 (d x)^m (a+b x^2+c x^4)^p.jl"
-    "1 Algebraic functions/1.2 Trinomial products/1.2.2 Quartic/1.2.2.3 (d+e x^2)^q (a+b x^2+c x^4)^p.jl"
-    # 1.2.2.4
-
-    "1 Algebraic functions/1.2 Trinomial products/1.2.3 General/1.2.3.1 (a+b x^n+c x^(2 n))^p.jl"
-    # 1.2.3.2, 1.2.3.3, 1.2.3.4
-
-    # 1.1.4.1, 1.1.4.2, 1.1.4.3
-
-    # 1.2.4.1, 1.2.4.2, 1.2.4.3, 1.2.4.4
-
-    # 1.4.1, 1.4.2
-
-    # 1.1.1.7
-    # 1.1.1.6
-    "1 Algebraic functions/1.1 Binomial products/1.1.1 Linear/1.1.1.5 P(x) (a+b x)^m (c+d x)^n.jl"
-
-    # 1.2 --- others
-
-    "2 Exponentials/2.1 (c+d x)^m (a+b (F^(g (e+f x)))^n)^p.jl"
-    "2 Exponentials/2.2 (c+d x)^m (F^(g (e+f x)))^n (a+b (F^(g (e+f x)))^n)^p.jl"
-    "2 Exponentials/2.3 Miscellaneous exponentials.jl"
-
-    "3 Logarithms/3.1/3.1.1 (a+b log(c x^n))^p.jl"
-    "3 Logarithms/3.1/3.1.2 (d x)^m (a+b log(c x^n))^p.jl"
-    "3 Logarithms/3.1/3.1.3 (d+e x^r)^q (a+b log(c x^n))^p.jl"
-    "3 Logarithms/3.1/3.1.4 (f x)^m (d+e x^r)^q (a+b log(c x^n))^p.jl"
-    "3 Logarithms/3.1/3.1.5 u (a+b log(c x^n))^p.jl"
-    "3 Logarithms/3.2/3.2.1 (f+g x)^m (A+B log(e ((a+b x) over (c+d x))^n))^p.jl"
-    "3 Logarithms/3.2/3.2.2 (f+g x)^m (h+i x)^q (A+B log(e ((a+b x) over (c+d x))^n))^p.jl"
-    "3 Logarithms/3.2/3.2.3 u log(e (f (a+b x)^p (c+d x)^q)^r)^s.jl"
-    "3 Logarithms/3.3 u (a+b log(c (d+e x)^n))^p.jl"
-    "3 Logarithms/3.4 u (a+b log(c (d+e x^m)^n))^p.jl"
-    "3 Logarithms/3.5 Miscellaneous logarithms.jl"
-
-    "4 Trig functions/4.1 Sine/4.1.1/4.1.1.1 (a+b sin)^n.jl"
-    "4 Trig functions/4.1 Sine/4.1.1/4.1.1.2 (g cos)^p (a+b sin)^m.jl"
-    "4 Trig functions/4.1 Sine/4.1.1/4.1.1.3 (g tan)^p (a+b sin)^m.jl"
-    ]
-    load_all_rules([joinpath(@__DIR__, "rules/" * file) for file in rules_paths])
-    println("Loaded all $(length(rules)) rules")
+    load_all_rules([joinpath(@__DIR__, "rules/" * file) for file in all_rules_paths])
 end
 
 
@@ -137,7 +74,6 @@ function reload_rules(;verbose = false)
 
     load_all_rules()
     
-    println("Rules reloaded. Total rules: ", length(rules))
     if verbose
         println("Here they are in order:")
         for (i, rule) in enumerate(rules)
